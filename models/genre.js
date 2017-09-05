e49088d4ce1d80ee11a7c7d544e644ab0719814c
tlkg <tlkg1970@icloud.com>
2017-09-05 14:37:27 -0700

    add min and max length validation
    Genre, add min and max length validation

--- models/genre.js
+++ models/genre.js
@@ -4,6 +4,8 @@ var Schema = mongoose.Schema;
 
 var GenreSchema = Schema({
     name: {type: String, required: true, min: 3, max: 100}
+            minlength: [3, 'At least 3 characters for a genre.'],
+            maxlength: [100, 'Max length for genre is 100 characters.']
 });
 
 // Virtual for this genre instance URL
