0e367b09aec961252c0a29956b23fec92f4d81df
TLKG <TLKG1970@icloud.com>
2017-09-05 16:19:02 -0700

    fix minlength and maxlength due to typo.

--- models/genre.js
+++ models/genre.js
@@ -3,9 +3,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GenreSchema = Schema({
-    name: {type: String, required: true, min: 3, max: 100}
+    name: {type: String, required: true, 
             minlength: [3, 'At least 3 characters for a genre.'],
             maxlength: [100, 'Max length for genre is 100 characters.']
+    }
 });
 
 // Virtual for this genre instance URL
